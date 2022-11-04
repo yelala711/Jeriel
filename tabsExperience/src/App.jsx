480cd06720f860534c39791cbb4ab7abada5d391
Yel <jeriel.orias.21@usjr.edu.ph>
2022-11-04 10:51:33 +0800

    accordion
    edit css and jsx files

---
+++ tabsExperience/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Tabs from "./Tab";
+
+function App() {
+  return (
+    <div className="App">
+      <Tabs />
+    </div>
+  );
+}
+
+export default App;
