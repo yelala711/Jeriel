9ec4e99e4e795cbd65f64fe63ded1e3cbc183331
Yel <jeriel.orias.21@usjr.edu.ph>
2022-11-11 01:23:54 +0800

    update
    edited jsx files

--- groceryBud/src/App.jsx
+++ groceryBud/src/App.jsx
@@ -2,56 +2,73 @@ import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 
-
-
 function App() {
-  const [input, setInput] = useState('');
-  const [isEmpty, setEmptyState] = useState(null);
-  const [buttonName, setButtonName] = useState('Submit');
-  const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [name, setName] = useState("");
+  const [allData, setAllData] = useState([]);
+  const [show, setShow] = useState(false);
+  const [editIndex, setEditIndex] = useState();
 
-  const handleClick = event => {
-    event.preventDefault();
-
-    if (input.trim().length !== 0) {
-      setEmptyState(false);
-    } else{
-      setEmptyState(true);
+  const handleAdd = () => {
+    if (name.length !== 0) {
+      setAllData((newData) => [...newData, name]);
+      setName("");
     }
   };
 
-  const handleSubmit = (e)=>{
-    e.preventDefault();
-    console.log (name)
-    const date = (name)
+  const handleDelete = (index) => {
+    allData.splice(index, 1);
+    setAllData([...allData]);
+  };
 
-    if (name){
-      setList((ls)=>[...ls, date])
-      setName("")
-    }
-  }
+  const handleEdit = (i) => {
+    setName(allData[i]);
+    setShow(true);
+    setEditIndex(i);
+  };
+
+  const handleUpdate = () => {
+    allData.splice(editIndex, 1, name);
+    setAllData([...allData]);
+    setShow(false);
+    setName("");
+  };
 
   return (
     <div>
       <h2>Grocery Hub</h2>
 
-      {
-        isEmpty ?
-          <p>Please Enter Item To Add To Basket</p> : <p>"{name}" Added to Basket</p>
-      }
+      <input
+        name="name"
+        placeholder="e.g. Eggs"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
-      <form onSubmit = {handleSubmit}>
-          <input id="name" name="name" placeholder = "e.g. Eggs" value={name} onChange={(e)=>setName(e.target.value)}/>
-          <input type="submit" name="buttonSub" value={buttonName}/>
-      </form>
+      {!show ? (
+        <button onClick={handleAdd}>Add</button>
+      ) : (
+        <button onClick={handleUpdate}>Update</button>
+      )}
 
-      {
-        list.map((a)=><div>
-          <li>{a.name}</li>
-        </div>)
-      }
-      
+      {allData.map((val, i) => (
+        <div>
+          <tr>
+            <td>
+              <p>{val}</p>
+            </td>
+            <td>
+              <button className="edit" onClick={() => handleEdit(i)}>
+                Edit
+              </button>
+            </td>
+            <td>
+              <button className="delete" onClick={() => handleDelete(i)}>
+                Delete
+              </button>
+            </td>
+          </tr>
+        </div>
+      ))}
     </div>
   );
 }
