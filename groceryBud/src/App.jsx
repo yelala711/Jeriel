9c9038688edcedbbe489c549756bcdc8fe2dc925
Yel <jeriel.orias.21@usjr.edu.ph>
2022-11-12 00:50:22 +0800

    improved css
    edited css files

--- groceryBud/src/App.jsx
+++ groceryBud/src/App.jsx
@@ -1,21 +1,34 @@
-import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
   const [name, setName] = useState("");
   const [allData, setAllData] = useState([]);
   const [show, setShow] = useState(false);
   const [editIndex, setEditIndex] = useState();
-  const [notif, setNotif] = useState(" ");
+  const [empty, setEmpty] = useState(true);
+  const [updated, setUpdated] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
+  const [newItem, setNewItem] = useState("");
+  const [clear, setClear] = useState(false);
 
   const handleAdd = () => {
     if (name.length !== 0) {
       setAllData((newData) => [...newData, name]);
-      setNotif({ name } + " is added in the basket");
       setName("");
+      setEmpty(false);
+      setNewItem(name);
+      setShowMessage(true);
+      setTimeout(() => {
+        setShowMessage(false);
+      }, 2000);
     } else if (name.length === 0) {
-      setNotif("Empty Input");
+      setEmpty(true);
+      setShowMessage(true);
+      setTimeout(() => {
+        setShowMessage(false);
+      }, 2000);
     }
   };
 
@@ -35,18 +48,72 @@ function App() {
       allData.splice(editIndex, 1, name);
       setAllData([...allData]);
       setShow(false);
-      setNotif({ name } + " is added in the basket");
+      setEmpty(false);
       setName("");
+      setNewItem(name);
+      setUpdated(true);
+      setTimeout(() => {
+        setUpdated(false);
+      }, 2000);
     } else if (name.length === 0) {
-      setNotif("Empty Input");
+      setEmpty(true);
+      setNewItem(name);
+      setUpdated(true);
+      setTimeout(() => {
+        setUpdated(false);
+      }, 2000);
     }
+  };
+
+  const handleClear = () => {
+    setAllData([]);
+    setClear(true);
+    setTimeout(() => {
+      setClear(false);
+    }, 2000);
   };
 
   return (
     <div>
       <h2>Grocery Hub</h2>
 
-      {notif !== "" ? <p> {notif} </p> : NULL}
+      {showMessage ? (
+        empty ? (
+          <p class="notification" id="danger">
+            Please Enter Item To Add To Basket
+          </p>
+        ) : (
+          <p class="notification" id="added">
+            {" "}
+            '{newItem}' is added to the basket
+          </p>
+        )
+      ) : (
+        ""
+      )}
+
+      {updated ? (
+        empty ? (
+          <p class="notification" id="danger">
+            Please Enter Item To Add To Basket
+          </p>
+        ) : (
+          <p class="notification" id="updated">
+            {" "}
+            '{newItem}' is updated in the basket
+          </p>
+        )
+      ) : (
+        ""
+      )}
+
+      {clear ? (
+        <p class="notification" id="clear">
+          Cleared
+        </p>
+      ) : (
+        ""
+      )}
 
       <input
         name="name"
@@ -62,24 +129,36 @@ function App() {
       )}
 
       {allData.map((val, i) => (
-        <div>
+        <div class="item">
           <tr>
             <td>
-              <p>{val}</p>
+              <p class="itemName">{val}</p>
             </td>
             <td>
-              <button className="edit" onClick={() => handleEdit(i)}>
+              <button
+                class="editBTN"
+                className="edit"
+                onClick={() => handleEdit(i)}
+              >
                 Edit
               </button>
             </td>
             <td>
-              <button className="delete" onClick={() => handleDelete(i)}>
+              <button
+                class="deleteBTN"
+                className="delete"
+                onClick={() => handleDelete(i)}
+              >
                 Delete
               </button>
             </td>
           </tr>
         </div>
       ))}
+      <br />
+      <button class="delete" className="clear" onClick={() => handleClear()}>
+        Clear All
+      </button>
     </div>
   );
 }
